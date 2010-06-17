3972cb574ccd7938737aef3742ed339b28bbfdd9
Jeff Licquia <jeff@licquia.org>
2010-06-17 11:42:45 -0400

    Add License link in documentation.

--- compliance/linkage/views.py
+++ compliance/linkage/views.py
@@ -161,6 +161,11 @@ def changelog(request):
     from site_settings import gui_name, gui_version
     return render_to_response('linkage/changelog.html', {'name': gui_name, 'version': gui_version})
 
+# license page
+def license(request):
+    from site_settings import gui_name, gui_version
+    return render_to_response('linkage/license.html', {'name': gui_name, 'version': gui_version})
+
 ### utility functions
 
 # pre-render the table data for the detail page
