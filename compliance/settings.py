d5f293878dec149a86b855fa7e2db3887cede649
Jeff Licquia <licquia@linuxfoundation.org>
2017-08-30 21:14:09 -0400

    Refactor how the app finds itself.

--- compliance/settings.py
+++ compliance/settings.py
@@ -1,14 +1,22 @@
 # Django settings for compliance project.
 
-import os, time
+import os
+import os.path
+import time
 
 # Function for finding the project root.
 
 def get_project_root():
-    project_root_paths = [ ".", "..", "/opt/linuxfoundation" ]
-    for path in project_root_paths:
-        if os.path.exists(os.path.join(path, "bin/readelf.py")):
-            return path
+    current_path = os.path.abspath(__file__)
+    while current_path != "/":
+        (current_path, fn) = os.path.split(current_path)
+        if os.path.basename(current_path) != "local" and \
+           os.path.exists(os.path.join(current_path, "bin/readelf.py")):
+            return current_path
+
+    # If that doesn't work, try an old common path.
+    if os.path.exists("/opt/linuxfoundation/bin/readelf.py"):
+        return "/opt/linuxfoundation"
 
     # Shouldn't get here unless we can't find the path.
     raise RuntimeError, "could not find the project path"
@@ -28,6 +36,17 @@ def use_userdir():
         return False
 
     return True
+
+# Find the doc root.
+
+def get_doc_root(candidate_paths, default_path):
+    docdir = None
+    for d in candidate_paths:
+        if os.path.exists(os.path.join(d, "docs/index.html")):
+            docdir = d
+            break
+
+    return (docdir and docdir or default_path)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -86,8 +105,6 @@ else:
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = '' 
 
-STATIC_DOC_ROOT = os.path.join(PROJECT_ROOT, 'compliance/media')
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
@@ -97,6 +114,7 @@ MEDIA_URL = ''
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/'
+STATIC_URL = '/site_media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'wzb!69=4(kj=w)vl&lyp-j1ff9#fi8^)p^i4xr9$kokcu5j9pk'
@@ -120,8 +138,17 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), "templates"),
     os.path.join(PROJECT_ROOT, "compliance/templates"),
 )
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), "media"),
+    os.path.join(PROJECT_ROOT, "compliance/media"),
+)
+
+STATIC_DOC_ROOT = get_doc_root(list(STATICFILES_DIRS),
+                               os.path.join(PROJECT_ROOT, "compliance/media"))
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -129,5 +156,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'django.contrib.staticfiles',
     'compliance.linkage',
 )
